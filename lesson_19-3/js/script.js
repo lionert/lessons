05eaf03b59518513767ecd8dd67eb619251fe190
lionert <112777786+lionert@users.noreply.github.com>
2024-07-01 18:36:11 +0200

    Add files via upload

---
+++ lesson_19-3/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+  if (targetElement.closest(".menu-icon")) {
+    document.documentElement.classList.toggle("menu-open");
+  }
+});
